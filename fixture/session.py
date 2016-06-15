b9b7e5ef89519d79cb333597ae356918c83903bc
bondarenko <anna.kovsh@gmail.com>
2016-06-16 00:11:54 +0300

    commit after 5-th lesson. parameteresation

--- fixture/session.py
+++ fixture/session.py
@@ -11,25 +11,29 @@ class SessionHelper:
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_logget_user() == username
+
+    def get_logget_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
+
     def ensure_logout(self):
         wd = self.app.wd
         if self.is_logged_in():
             self.logout()
-
-    def is_logged_in(self):
-        wd = self.app.wd
-        return len(wd.find_element_by_link_text("Logout")) > 0
-
-    def is_logged_in_as(self, username):
-        wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
 
     def ensure_login(self, username, password):
         wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10 # + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = \
+    [
+        Group(name="11111", header="1111111", footer="1111111"),
+        Group(name="", header="", footer="")
+    ]+[
+        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+        for i in range(5)
+    ]
+#+[Group(name=name, header=header, footer=footer)
+#  for name in ["", random_string("name", 10)]
+#for header in ["", random_string("header", 20)]
+#for footer in ["", random_string("footer", 20)]
+#  ]
 
 
-
-def test_add_valid_group(app):
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_valid_group(app, group):
     old_group = app.group.get_group_list()
     group = Group(name="11111", header="1111111", footer="1111111")
     app.group.create(group)
@@ -13,18 +33,19 @@ def test_add_valid_group(app):
     new_group = app.group.get_group_list()
 
     old_group.append(group)
+
     assert sorted(old_group, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
 
 
 
-def test_add_empty_group(app):
-    old_group = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_group = app.group.get_group_list()
-    assert len(old_group) + 1 == len(new_group)
-    old_group.append(group)
-    assert sorted(old_group, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_group = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_group = app.group.get_group_list()
+#    assert len(old_group) + 1 == len(new_group)
+#    old_group.append(group)
+#    assert sorted(old_group, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
 
 
 
